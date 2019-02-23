7dd868d3681747aefecff7ccd8dce436abce2f40
mbaumaister13 <mbaumaister@ufl.edu>
2019-02-22 19:26:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# slack-reaction-bot
+Slack bot which adds reactions to messages 
+
+#Get Started
+- Put your slack API key in a permissions.txt
+```
+python get_bot_id.py
+python main.py
+```
+#Future
+- Add automated testing modules
+```
+https://docs.python.org/3/library/doctest.html#module-doctest
+```
+
+pip -r requirements.txt
---
+++ emoji_parser.py
@@ -0,0 +1,56 @@
+import json
+
+
+class EmojiParser:
+
+    def __init__(self, slack_client):
+        # grabs the custom emoji list for server
+        self.custom_emoji_list = slack_client.api_call("emoji.list")["emoji"]
+        self.users_list = slack_client.api_call("users.list")["members"]
+        # load emojis file
+        with open('emoji.json') as data_file:
+            self.data = json.load(data_file)
+
+    def search_list(self, query_list):
+        result_set = list()
+        for query in query_list:
+            if query in self.custom_emoji_list:
+                result_set.append(query)
+            result_set.append(self.find_query(query))
+        return result_set
+
+    def format_text(self, message):
+        formatted_message = message.lower()
+        return formatted_message
+
+    def find_query(self, query):
+        emoji = self.data["emoji"]
+        for sub_list in emoji:
+            if query == sub_list[0]:
+                return query
+            elif query == 'ok':
+                return 'ohkay'
+            elif query == 'f':
+                return 'letter_f'
+
+    # temporary function
+    def get_fields(self, output_list):
+        for output in output_list:
+            if 'subtype' in output:
+                if output['subtype'] == 'slackbot_response':
+                    return output['text'], output['channel'], output['ts'], output['user'], 'Slackbot'
+                else:    
+                    return output['text'], output['channel'], output['ts'], output['bot_id'], 'Some Bot'
+            else:
+                for users in self.users_list: 
+                    if output['user'] == users['id']:
+                        name = users['name']
+                return output['text'], output['channel'], output['ts'], output['user'], name
+    BOT_ID = 'UGDKW5T8F'
+
+    def parse_message(self, slack_message):
+        text, channel, timestamp, user, username = self.get_fields(slack_message)
+        text = self.format_text(text)
+        if text and channel and timestamp and user != self.BOT_ID:
+            emoji_list = self.search_list(text.split())
+            return emoji_list, channel, timestamp, user, username
