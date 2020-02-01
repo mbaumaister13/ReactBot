73b10d615bfc4d3cac50bbb78d5691d92e4990aa
Matt Baumaister <mbaumaister@ufl.edu>
2020-01-31 22:56:42 -0500

    Abstracted custom emoji dictionary to use a modifiable txt file

--- emoji_parser.py
+++ emoji_parser.py
@@ -10,6 +10,9 @@ class EmojiParser:
         #Grabs the custom emoji list and user list for server
         self.custom_emoji_list = slack_client.api_call("emoji.list")["emoji"]
         self.users_list = slack_client.api_call("users.list")["members"]
+        with open("emoji_dict.txt") as json_file:
+            self.emoji_dict = json.load(json_file)
+
 
     def search_list(self, query_list): #Adds found emoji matches to the result set
         result_set = set()
@@ -29,30 +32,14 @@ class EmojiParser:
         emoji_set = set(self.custom_emoji_list.keys())
         if query.startswith(':') and query.endswith(':'):
             return query[1:-1]
-        elif random.random() <= 0.65:
+        elif random.random() <= 1.0:
             if query in emoji_set:
                 for emote in difflib.get_close_matches(query, self.custom_emoji_list, 3, .85):
                        return emote
-            elif query == 'ok': #Emoji Dictionary here
-                return 'ohkay'
-            elif query in ['ty', 'thanks', 'thx', 'thank']:
-                return 'np'
-            elif query in ['peach', 'ass', 'booty', 'butt']:
-                return 'peach'
-            elif query in ['cock', 'dick', 'penis', 'dong']:
-                return 'eggplant'
-            elif query  in ['angry', '&gt;:(', 'd:&lt;', '):&lt;']:
-                return 'angryface'
-            elif query in ['hankey', 'poop', 'turd', 'shit', 'crap', 'feces', 'dookie', 'poopy', 'poopie', 'shid', 'shidded']:
-                return 'poop'
-            elif query in ['hehe', 'heehee']:
-                return 'xd'
-            elif query == 'corn':
-                return 'corn1'
-            elif query == 'ya':
-                return 'yeet'
+            for emoji in self.emoji_dict:
+                if query in self.emoji_dict[emoji]['list']:
+                    return emoji
 
-    # Ignore the shitty hardcoding
     def get_fields(self, output_list): #Gets fields for reaction adding
         for output in output_list:
             try:
@@ -63,22 +50,6 @@ class EmojiParser:
             except:
                 print("Failed to parse")
                 return None, None, None, None, None
-            # if 'subtype' in output and 'attachments' not in output:
-            #     if output['subtype'] in ['slackbot_response', 'channel_topic', 'me_message']:
-            #         return output['text'], output['channel'], output['ts'], output['user'], 'Event'
-            #     else:    
-            #         return output['text'], output['channel'], output['ts'], output['bot_id'], 'Some Bot'
-            # elif 'attachments' in output:
-            #     if output['username'] == 'Polly':
-            #         return 'Polly', output['channel'], output['ts'], output['bot_id'], 'Polly'
-            #     else:
-            #         print(output['attachments']['text'], output['channel'], output['ts'], output['user'], 'Quoted Message')
-            #         return 'Quoted Message', output['channel'], output['ts'], output['user'], 'Quoted Message'
-            # else:
-            #     for users in self.users_list: 
-            #         if output['user'] == users['id']:
-            #             name = users['name']
-            #     return output['text'], output['channel'], output['ts'], output['user'], name
 
     def parse_message(self, slack_message): #Parses each message sent
         text, channel, timestamp, user, username = self.get_fields(slack_message)
